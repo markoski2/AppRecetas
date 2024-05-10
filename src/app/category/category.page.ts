309b1f7f7379fa6cb932d7706cc697c55d409e2e
markoski2 <marco.alvarez.maao@gmail.com>
2024-05-09 19:30:29 -0600

    new menu
    Se quito la categoria AlcoholicDrinks

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -32,33 +32,34 @@ export class CategoryPage implements OnInit {
 
     document.getElementById("Inicio")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
-      Content!.style.transform = "translateX(-14.2%)";
+      Content!.style.transform = "translateX(-16.6%)";
     })
     document.getElementById("Sopa")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
       this.ObjectGarnish=this.recipes.Garnish
-      Content!.style.transform = "translateX(-28.5%)";
+      Content!.style.transform = "translateX(-33.32%)";
     })
     document.getElementById("Comida")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
       this.ObjectMel=this.recipes.mel
-      Content!.style.transform = "translateX(-42.8%)";
+      Content!.style.transform = "translateX(-50%)";
     })
     document.getElementById("Postre")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
       this.ObjectDessert=this.recipes.Dessert
-      Content!.style.transform = "translateX(-57%)";
+      Content!.style.transform = "translateX(-66.68%)";
     })
     document.getElementById("Bebida")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
       this.ObjectDrink=this.recipes.beverege
-      Content!.style.transform = "translateX(-71.4%)";
+      Content!.style.transform = "translateX(-83.35%)";
     })
+    /*
     document.getElementById("BebidaAlcholica")?.addEventListener("click",()=>{
       var Content=document.getElementById("DivCarousel")
       this.ObjectAlcoholicDrinks=this.recipes.AlcoholicDrink
       Content!.style.transform = "translateX(-85.7%)";
-    })
+    })*/
     document.getElementById("ImgAdd")?.addEventListener("click",()=>{
       this.router.navigate(["/create-information"])
     })
